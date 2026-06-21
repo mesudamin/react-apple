edfb089db11b718b49390e94c511be02ddedbf33
mesudamin <lalemasud@gmail.com>
2026-06-21 17:56:03 +0300

    all file and folder

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./assets/components/HEADER/Header"
+import Footer from "./assets/components/Footer/Footer"
+import Sections from "./assets/components/sections/Sections.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+   <Header/>
+
+  <Sections/>
+
+      
+
+     <Footer/>
+    </>
+  )
+}
+
+export default App
