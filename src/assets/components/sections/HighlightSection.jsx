e6ffabe78d51c94c8e922b8d6f5d08cb9d1544e0
mesudamin <lalemasud@gmail.com>
2026-06-24 17:54:58 +0300

    changing to class base component

--- src/assets/components/sections/HighlightSection.jsx
+++ src/assets/components/sections/HighlightSection.jsx
@@ -1,24 +1,29 @@
-export default function HighlightSection(props) {
-  return (
-    <>
-      <div className={props.titleClass}>
-        {props.title}
-      </div>
+
+import  { Component } from 'react'
+
+export default class HighlightSection extends Component {
+  render() {
+    return (
+      <>
+         <div className={this.props.titleClass}>
+         {this.props.title}
+       </div>
 
       <div className="description-wrapper">
-        {props.description}
-      </div>
+        {this.props.description}
+       </div>
 
       <div className="price-wrapper">
-        {props.price}
-      </div>
+        {this.props.price}
+     </div>
 
       <div className="links-wrapper">
-        <ul>
-          <li><a href="">{props.link1}</a></li>
-          <li><a href="">{props.link2}</a></li>
-        </ul>
-      </div>
-    </>
-  );
-} 
+          <ul>
+           <li><a href="">{this.props.link1}</a></li>
+            <li><a href="">{this.props.link2}</a></li>
+          </ul>
+        </div>
+      </>
+    )
+  }
+}
